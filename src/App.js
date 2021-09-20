0cf7129a2835bdd293a03a5f8e399c1c49a3cbea
Yellow <ilya.753753@gmail.com>
2021-09-20 19:42:56 +0500

    css fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
-import './app.css'
+import './App.css'
 import ImgForm from './componets/ImgForm';
 
 function App() {
